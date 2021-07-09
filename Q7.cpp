ad54ffbe9f6ac359ee486d9519d7d083ef9ef328
qii1120 <87166290+qii1120@users.noreply.github.com>
2021-07-09 14:07:53 +0800

    Create Q7.cpp

---
+++ Q7.cpp
@@ -0,0 +1,13 @@
+class bar3d
+{
+  public:
+  	int width,height,depth;
+  	bar3d(int w,int h, int d){
+      if(w>0&&h>0&&d>0&&w<1000&&h<1000&&d<1000){
+        width=w,height=h,depth=d;
+      }
+      else{
+        width=0,height=0,depth=0;
+      }
+    }
+};
